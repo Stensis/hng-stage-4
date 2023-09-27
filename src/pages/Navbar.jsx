0ee7085ead2dbca759936c6c58859b70f1e73803
Stensis <irenenjuguna98@gmail.com>
2023-09-27 19:45:09 +0300

    modified

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = ({ isPageTop, selectedPage, setSelectedPage }) => {
   return (
     <nav className={`${navBackground} z-40 w-full fixed top-0 py-6`}>
       <div className='flex items-center justify-between mx-auto w-5/6'>
-        <h4 className=' font-playfair text-3xl  font-bold'>Karrey</h4>
+        <h4 className=' font-playfair text-3xl  font-bold'>Software developer</h4>
 
         {/* Desktop Nav  */}
         {isAboveSmallScreen ? (
